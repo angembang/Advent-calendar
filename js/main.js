6f9ae11d40c850c1813ef62ddc0c5939e976697c
Ange KAMWANG MBANG <ange.kamwangmbang2025@campus-eni.fr>
2025-05-09 11:14:42 +0200

    Added the import of the index.js file

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-import {register} from "./modules/register.js";
-import {login} from "./modules/login.js";
-import {home} from "./modules/home.js";
+import { register } from "./modules/register.js";
+import { login } from "./modules/login.js";
+import { home } from "./modules/home.js";
+import { image } from "./modules/image.js";
+import { media } from "./modules/media.js";
+import { quote } from "./modules/citation.js";
+import { year } from "./modules/index.js";
 document.addEventListener("DOMContentLoaded", () => {
-
-    register();
-    login();
-    home();
-    
-})
+  register();
+  login();
+  home();
+  image();
+  media();
+  quote();
+  const currentYear = document.getElementById("currentYear");
+  if (currentYear) {
+    year();
+  }
+});
